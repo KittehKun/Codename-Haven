2edd7e911d92bf9de0f34b1bf4d55c5b6cc5c75e
Zach H <huntzachary30@gmail.com>
2023-10-02 03:14:31 -0500

    Created all purpose interior teleport script

--- Interior Teleports/EnterInterior.cs
+++ Interior Teleports/EnterInterior.cs
@@ -7,11 +7,11 @@ using VRC.Udon;
 //The purpose of this script is to handle teleport players to the interior of a location. A general purpose script.
 public class EnterInterior : UdonSharpBehaviour
 {
-    public Vector3 targetLocation; //Location of where the player will spawn inside an interior | Assigned in Unity inspector - different location needs to be set for every interior
+    public Transform targetLocation; //Location of where the player will spawn inside an interior | Assigned in Unity inspector - different location needs to be set for every interior
 
     public override void Interact()
     {
         //Set the player's position to the target location
-        Networking.LocalPlayer.TeleportTo(targetLocation, Networking.LocalPlayer.GetRotation());
+        Networking.LocalPlayer.TeleportTo(targetLocation.position, targetLocation.rotation);
     }
 }
